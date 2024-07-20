ff2bdd0a8830d72ddf59c891c09d7ad516226ff7
Joshuawuro <joshuawuro@gmail.com>
2024-07-19 21:28:59 -0700

    update script,js

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    function addTask(){
+        const taskText = document.getElementById('task-input').value.trim();
+        
+        if (isEmpty(taskInput)){
+            alert('Enter a new task');
+        } else {
+            const listItem = document.createElement('li');
+
+            listItem.textContent = taskText;
+        }
+
+        const newButton = document.createElement('button');
+        newButton.textContent = 'Remove';
+        newButton.setAttribute('class', 'remove-btn');
+
+        newButton.onclick = function(){
+            listItem = '';
+        }
+    }
+
+    
+});
